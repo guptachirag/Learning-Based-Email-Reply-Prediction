b89d4343159e9215729d5c40e2b7add897eb7673
Chirag Gupta <chiragguptadtu@gmail.com>
2014-05-09 22:18:45 -0700

    FeatureExtractor.py added

--- FeatureExtractor.py
+++ FeatureExtractor.py
@@ -1,18 +1,21 @@
 import email
 from bs4 import BeautifulSoup
 import EmailFetcher as ef
+import Classifier as cf
 from pprint import pprint
 
 CHIRAG = "chiragguptadtu@gmail.com"
 
 interrogative_words=["could", "which", "what" , "whose" , "who", "whom" ,"where", "when" ,"how" ,"why", "wherefore" ,"whether"]
-negative_words=["unsubscribe","no-reply","noreply","mailer"]
+negative_words=["unsubscribe","no-reply","noreply","mailer","automated"]
 
 
 def words_present(all_text,keyword_list):   #checks if any word in all_text is present in keyword_list
-    if any(word in all_text for word in keyword_list):
-        return True
-    return False
+	if all_text == None :
+		return False
+	if any(word in all_text for word in keyword_list) :
+		return True
+	return False
 
 
 def extract_text( email_message_instance):
@@ -29,37 +32,32 @@ def extract_text( email_message_instance):
 
 def parseEmail(emailString):
     message = email.message_from_string(emailString)
-
-    feature={}
-    feature["From"]=email.utils.parseaddr(message['From'])[1]
-    feature["Subject"]=message['Subject']
-    feature["is_automated_mail"]=words_present(feature["From"],negative_words)
-    feature["Message-ID"]=message["Message-ID"]
-    feature["reply"] = False
+    
+    mail={}
+    mail["From"]=email.utils.parseaddr(message['From'])[1]
+    mail["Subject"]=message['Subject']
+    mail["Message-ID"]=message["Message-ID"]
 
     if "Cc" in message.keys():
-        feature["Cc"] =  True
+        mail["Cc"] =  1
     else:
-        feature["Cc"] =  False
+        mail["Cc"] =  0
 
     if "In-Reply-To" in message.keys():
-        feature["In-Reply-To"] =  message["In-Reply-To"];
+        mail["In-Reply-To"] =  message["In-Reply-To"];
 
     emailText = extract_text(message)
 
-    #print "From : " +   feature["From"]
+    #print "From : " +   mail["From"]
     #print "To : " + message['To']
     #print "Subject : " + message['Subject']
     #print "TEXT BODY : "
     if emailText != None:
         emailText = BeautifulSoup(emailText).get_text()
-        #print emailText
-        #feature["emailText"]=emailText
-        feature["is_interrogative_text"]=words_present(emailText,interrogative_words)
-    else:
-        feature["is_interrogative_text"]=False
+    #print emailText
+    mail["emailText"]=emailText
 
-    return feature
+    return mail
 
 
 def read_from_disk(directory):
@@ -76,27 +74,59 @@ def read_from_disk(directory):
     return emails
 
 
+def senderFrequency(sender,emails):
+	cnt=0
+	for email in emails:
+		if email["From"] == sender:
+			cnt=cnt+1
+	return cnt
+	
+
+def replied(messageid,sentEmails):
+	for email in sentEmails:
+		if messageid == email["In-Reply-To"]:
+			return 1
+	return 0
+
+
+def dictList(dictionary):
+	ret = []
+	for i in dictionary.keys():
+		ret.append(dictionary[i])
+	return ret
+
 def extractFeatures():
     emails = read_from_disk(ef.INBOX_DIRECTORY)
 
-    inboxEmailFeatures = []
-    sentEmailFeatures = []
+    inboxEmails = []
+    sentEmails = []
 
+	#seprate sent and inbox emails
     for email in emails:
-        emailFeatures = parseEmail(email)
-        if emailFeatures["From"] == CHIRAG:
-            sentEmailFeatures.append(emailFeatures)
+        email = parseEmail(email)
+        if email["From"] == CHIRAG:
+            sentEmails.append(email)
         else :
-            inboxEmailFeatures.append(emailFeatures)
-
-    #classify the emails
-    for smail in sentEmailFeatures :
-        if "In-Reply-To" in smail.keys():
-            for imail in inboxEmailFeatures :
-                if imail["Message-ID"] == smail["In-Reply-To"]:
-                    imail["reply"]=True
-
-
-    for i in inboxEmailFeatures :
-        print "\n\n-----------------------------------------------------------\n"
-        pprint(i)
+            inboxEmails.append(email)
+                   
+    inboxEmailFeatures = []
+    output = []
+    
+	# find numerical features
+    for email in inboxEmails :
+    	features = {}
+    	features["sender_frequency"] = senderFrequency(email["From"],inboxEmails)
+    	features["is_automated_mail"] = words_present(email["emailText"],negative_words)
+    	features["reply"] = replied(email["Message-ID"],sentEmails)
+    	features["is_interrogative_text"] = words_present(email["emailText"],interrogative_words)
+    	features["Cc"]=email["Cc"]
+    	featureslist = dictList(features)
+    	output.append(features["reply"])
+    	inboxEmailFeatures.append(featureslist)
+    
+    result = cf.randomForrestClassifier(inboxEmailFeatures,output,inboxEmailFeatures)
+   	 	
+    #for i in inboxEmailFeatures :
+	   	#print "\n\n-----------------------------------------------------------\n"
+	   	#pprint(i)
+        
